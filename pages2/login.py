9250e90f47c09a8cc8b202963d1b11732ed84316
InduManimaran <indu3102@gmail.com>
2025-06-09 11:13:19 +0530

    login.py
    center the login page

--- pages2/login.py
+++ pages2/login.py
@@ -4,18 +4,25 @@ from utils import check_login
 
 #def login():
 def run():
-    st.title("Login")
+    st.markdown('<div class="centered">', unsafe_allow_html=True)
 
-    username = st.text_input("Username")
-    password = st.text_input("Password", type="password")
-    login_btn = st.button("Login")
+    with st.container():
+        st.markdown('<div class="login-box">', unsafe_allow_html=True)
+        st.title("Login")
 
-    if login_btn:
-        if check_login(username, password):
-            st.session_state["logged_in"] = True
-            st.session_state.authenticated = True 
-            st.session_state["username"] = username
-            st.success("Logged in successfully")
-            st.rerun()
-        else:
-            st.error("Invalid username or password")
+        with st.form("login_form", clear_on_submit=False):
+            username = st.text_input("Username")
+            password = st.text_input("Password", type="password")
+            login_btn = st.form_submit_button("Login")
+
+        if login_btn:
+            if check_login(username, password):
+                st.session_state["logged_in"] = True
+                st.session_state.authenticated = True 
+                st.session_state["username"] = username
+                st.success("Logged in successfully")
+                st.rerun()
+            else:
+                st.error("Invalid username or password")
+        st.markdown("</div>", unsafe_allow_html=True)
+    st.markdown("</div>", unsafe_allow_html=True)
