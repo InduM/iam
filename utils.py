e6d364244523820b342a4d9f7ae634c9e44bc162
InduManimaran <indu3102@gmail.com>
2025-06-10 11:28:20 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,7 +1,8 @@
 import streamlit as st
 # utils.py
 def check_login(username, password):
-    return username == "admin" and password == "1234"
+    #return username == "admin" and password == "1234"
+    return True
 
 def logout_user():
     st.session_state.clear()
@@ -14,7 +15,8 @@ def is_logged_in():
 def login_user(username, password):
     # Replace this with your auth logic
     #if username == "admin" and password == "1234":
-    #    st.session_state["logged_in"] = True
+    #   st.session_state["logged_in"] = True
     #    return True
     #return False
+    st.session_state["logged_in"] = True
     return True
